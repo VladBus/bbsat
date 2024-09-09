385478e04b0861605e67fc54e67abd79e9d52ea2
Vlad Busev <buvlad2002@gmail.com>
2024-09-09 13:57:20 +0300

    :hammer: Again fixed bat file
    Исправил бат файл полностью так как он не хотел работать, есть подозрение на то что он запускал и сразу закрывал main.

--- run_bbsat.bat
+++ run_bbsat.bat
@@ -1,1 +1,9 @@
-"E:\Programming_Work\Pycharm_Work\bbsat\.venv\Scripts\python.exe" main.py
+@echo off
+
+echo Запускаем скрипт main.py...
+
+"E:\Programming_Work\Pycharm_Work\bbsat\.venv\Scripts\python.exe" "E:\Programming_Work\Pycharm_Work\bbsat\main.py"
+
+echo Скрипт main.py завершен.
+
+exit
