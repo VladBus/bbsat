53c2eceec97974253db195905ea7b544e96f05f9
Vlad Busev <buvlad2002@gmail.com>
2024-09-24 14:28:53 +0300

    :nut_and_bolt: Fixed again .bat file and main.
    Снова исправлял бат файл и мэйн думою что уже наконец разобрался со всеми проблемами для автоматического запуска.

--- main.py
+++ main.py
@@ -18,25 +18,29 @@ logging.basicConfig(
     format='%(asctime)s - %(levelname)s - %(filename)s - %(lineno)d - %(message)s'
 )
 
-# Задачи расписания
+# Запланированные задачи
 schedule.every().day.at("00:05").do(lambda: run_script(SORT_FILES_PATH))
 schedule.every().day.at("00:50").do(lambda: run_script(TIFF_COMPRESSOR_PATH))
 schedule.every().wednesday.at("02:00").do(lambda: run_script(STATISTIC_PATH))
 schedule.every().wednesday.at("02:45").do(lambda: run_script(SEND_MESSAGE_PATH))
 
 # Завершение программы в разное время для разных дней недели
-schedule.every().wednesday.at("03:00").do(lambda: exit())  # Среда
-schedule.every().day.at("02:10").do(lambda: exit())  # Все остальные дни
+schedule.every().wednesday.at("03:00").do(lambda: exit(0))  # Среда
+schedule.every().day.at("02:10").do(lambda: exit(0))  # Все остальные дни
 
 
 # Функция для запуска скриптов
 def run_script(script_path):
+    logging.info(f"Проверка существования скрипта: {script_path}")
     if not os.path.isfile(script_path):
         logging.error(f"Скрипт не найден: {script_path}")
         return
+
     try:
         logging.info(f"Запуск скрипта: {script_path}")
         result = subprocess.run(["python", script_path], capture_output=True, text=True)
+
+        # Проверяем результат выполнения
         if result.returncode == 0:
             logging.info(f"Скрипт {script_path} успешно завершён.")
             if result.stdout:
@@ -50,5 +54,6 @@ def run_script(script_path):
 
 # Бесконечный цикл для выполнения задач
 while True:
+    logging.info("Проверка и выполнение запланированных задач.")
     schedule.run_pending()
     time.sleep(1)
--- run_bbsat.bat
+++ run_bbsat.bat
@@ -1,9 +1,13 @@
 @echo off
 
-echo Starts skript main.py...
+echo Starts skript main.py at %date% %time%... >> "S:\message\logofile\bat_log.txt"
 
 "E:\Programming_Work\Pycharm_Work\bbsat\.venv\Scripts\python.exe" "E:\Programming_Work\Pycharm_Work\bbsat\main.py" >> "S:\message\logofile\bat_log.txt" 2>&1
 
-echo Skript main.py end.
+if %ERRORLEVEL% NEQ 0 (
+    echo Ошибка при выполнении main.py. Код ошибки: %ERRORLEVEL% >> "S:\message\logofile\bat_log.txt"
+)
 
-exit
+echo Skript main.py end at %date% %time% >> "S:\message\logofile\bat_log.txt"
+
+exit /b %ERRORLEVEL%
